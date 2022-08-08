133d7d1c55566ffb9babd42d8d9e90789a23e22a
Kanav Chopra <84139025+KanavChopra@users.noreply.github.com>
2022-08-09 02:41:50 +0530

    Time: 7 ms (82.43%), Space: 13.6 MB (78.16%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,17 +1,16 @@
-// Tabulation DP
+// Space Optimized 
 
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        vector<int> dp(n + 1, -1);
-        dp[0] = cost[0];
-        dp[1] = cost[1];
+        int a = cost[0];
+        int b = cost[1];
         for (int i = 2; i < n; ++i) {
-            dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i];
+            int curr = min(a, b) + cost[i];
+            a = b;
+            b = curr;
         }
-        // I can reach the top by either coming from (n - 1)th step
-        // or (n - 2)th step. So we have to return min of that.
-        return min(dp[n - 1], dp[n - 2]);
+        return min(a, b);
     }
 };
