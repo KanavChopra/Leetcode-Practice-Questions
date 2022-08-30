20f51358a6bcd9fbcbb0237ebfe3badf1d97f1c9
Kanav Chopra <84139025+KanavChopra@users.noreply.github.com>
2022-08-31 04:11:13 +0530

    Time: 8 ms (9.60%), Space: 7.1 MB (73.72%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        int left = 0;
+        int right = cols - 1;
+        while (left < right) {
+            for (int i = 0; i < right - left; ++i) {
+                int top = left;
+                int bottom = right;
+                int temp = matrix[top][left + i];
+                matrix[top][left + i] = matrix[bottom - i][left];
+                matrix[bottom - i][left] = matrix[bottom][right - i];
+                matrix[bottom][right - i] = matrix[top + i][right];
+                matrix[top + i][right] = temp;
+            }
+            ++left;
+            --right;
+        }
+    }
+};
