2936f7fd867dab2f1173bed1d78ab66236421ea8
Kanav Chopra <84139025+KanavChopra@users.noreply.github.com>
2022-09-02 02:14:54 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (57.13%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,21 +1,18 @@
+// This question is similar to Fibonacci Number so doing only space optimized approach
+
 class Solution {
 public:
-    int solve(int n, vector<int>& dp) {
-        if (n == 1) {
-            return 1;
-        }
-        if (n == 2) {
-            return 2;
-        }
-        if (dp[n] != -1) {
-            return dp[n];
-        }
-        dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
-        return dp[n];
-    }
     int climbStairs(int n) {
-        vector<int> dp(n + 1, -1);
-        int ans = solve(n, dp);
-        return ans;
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        int a = 1;
+        int b = 1;
+        for (int i = 2; i <= n; ++i) {
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
     }
 };
