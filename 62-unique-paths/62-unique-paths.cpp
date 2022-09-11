cc3ff1aa80a23976c3c3cdf84d65bb6dd4f51eec
Kanav Chopra <84139025+KanavChopra@users.noreply.github.com>
2022-09-11 22:32:47 +0530

    Time: 4 ms (28.88%), Space: 6.5 MB (46.70%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,18 +1,19 @@
-/*
-    Reference:
-        https://leetcode.com/problems/unique-paths/discuss/1581998/C%2B%2BPython-5-Simple-Solutions
-        -w-Explanation-or-Optimization-from-Brute-Force-to-DP-to-Math
-
-*/
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, 1));
-        for (int i = 1; i < m; ++i) {
-            for (int j = 1; j < n; ++j) {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-            }
+    int solve(int i, int j, vector<vector<int>> &dp) {
+        if (i == 0 && j == 0) {
+            return 1;
         }
-        return dp[m - 1][n - 1];
+        if (i < 0 or j < 0) {
+            return 0;
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        return dp[i][j] = solve(i - 1, j, dp) + solve(i, j - 1, dp);
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return solve(m - 1, n - 1, dp);
     }
 };
