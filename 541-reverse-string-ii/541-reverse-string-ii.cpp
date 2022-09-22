88c69944ae6fb70e941bb1dd1014d01ba9ec7059
Kanav Chopra <84139025+KanavChopra@users.noreply.github.com>
2022-09-22 23:24:03 +0530

    Time: 21 ms (5.93%), Space: 7.1 MB (98.84%) - LeetHub

---
+++ 541-reverse-string-ii/541-reverse-string-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int start = 0;
+        int end = min((int)s.length(), k);
+        while (start < s.length()) {
+            reverse(s.begin() + start, s.begin() + end);
+            start += 2 * k;
+            end = min((int)s.length(), start + k);
+        }
+        return s;
+    }
+};
