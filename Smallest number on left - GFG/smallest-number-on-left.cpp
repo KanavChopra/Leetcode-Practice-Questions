c6a25bf45322797fe423401607457123f03afd40
Kanav Chopra <84139025+KanavChopra@users.noreply.github.com>
2022-11-21 17:39:25 +0530

    Added solution - LeetHub

---
+++ Smallest number on left - GFG/smallest-number-on-left.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    vector<int> leftSmaller(int n, int a[]) {
+        vector<int> res(n);
+        stack<int> st;
+        for (int i = 0; i < n; ++i) {
+            while (!st.empty() && st.top() >= a[i]) {
+                st.pop();
+            }
+            if (st.empty()) {
+                res[i] = -1;
+            } else {
+                res[i] = st.top();
+            }
+            st.push(a[i]);
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i = 0;i < n;i++)
+            cin>>a[i];
+        
+        Solution ob;
+        vector<int> ans = ob.leftSmaller(n, a);
+        for(int i = 0;i < n;i++)
+            cout<<ans[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
