33f0b70b6ca3494b31fa696fc7379a5d052a8016
Kanav Chopra <84139025+KanavChopra@users.noreply.github.com>
2023-01-21 00:25:12 +0530

    Time: 4 ms (75.09%), Space: 9.7 MB (56.02%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int getNextDayIndex(int dayIndex, vector<int>& days, int duration) {
+        int endDay = days[dayIndex] + duration - 1;
+        int nextDay = dayIndex;
+        while (nextDay < days.size() && days[nextDay] <= endDay) {
+            nextDay++;
+        }
+        return nextDay;
+    }
+    int solve(int i, vector<int>& days, vector<int>& costs, vector<int>& dp) {
+        if (i >= days.size()) {
+            return 0;
+        }
+        if (dp[i] != INT_MAX) {
+            return dp[i];
+        }
+        int totalCostDay = costs[0] + solve(getNextDayIndex(i, days, 1), days, costs, dp);
+        int totalCostWeek = costs[1] + solve(getNextDayIndex(i, days, 7), days, costs, dp);
+        int totalCostMonth = costs[2] + solve(getNextDayIndex(i, days, 30), days, costs, dp);
+        return dp[i] = min({totalCostDay, totalCostWeek, totalCostMonth});
+    }
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        vector<int> dp(days.size() + 1, INT_MAX);
+        return solve(0, days, costs, dp);
+    }
+};
