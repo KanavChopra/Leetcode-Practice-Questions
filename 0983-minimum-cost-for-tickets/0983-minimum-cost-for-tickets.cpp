413300bdc4edc6f4cd653f4b465e6332b7a566cd
Kanav Chopra <84139025+KanavChopra@users.noreply.github.com>
2023-01-21 00:35:38 +0530

    Time: 5 ms (55.37%), Space: 9.9 MB (22.67%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,27 +1,24 @@
+// Approach:
+// https://leetcode.com/problems/minimum-cost-for-tickets/discuss/762493/C%2B%2B-DP-Solution-w-Detailed-Explanation
+
 class Solution {
 public:
-    int getNextDayIndex(int dayIndex, vector<int>& days, int duration) {
-        int endDay = days[dayIndex] + duration - 1;
-        int nextDay = dayIndex;
-        while (nextDay < days.size() && days[nextDay] <= endDay) {
-            nextDay++;
-        }
-        return nextDay;
-    }
-    int solve(int i, vector<int>& days, vector<int>& costs, vector<int>& dp) {
-        if (i >= days.size()) {
-            return 0;
-        }
-        if (dp[i] != INT_MAX) {
-            return dp[i];
-        }
-        int totalCostDay = costs[0] + solve(getNextDayIndex(i, days, 1), days, costs, dp);
-        int totalCostWeek = costs[1] + solve(getNextDayIndex(i, days, 7), days, costs, dp);
-        int totalCostMonth = costs[2] + solve(getNextDayIndex(i, days, 30), days, costs, dp);
-        return dp[i] = min({totalCostDay, totalCostWeek, totalCostMonth});
-    }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        vector<int> dp(days.size() + 1, INT_MAX);
-        return solve(0, days, costs, dp);
+        unordered_set<int> travel(begin(days), end(days));
+        // dp[i] means upto i-th day what is the minimum cost of buying ticket
+        int dp[366] = {};
+        // dp[0] or day 0 represents day before any trip. so we shouldn't try
+        // to minimize it. so we set it to 0
+        dp[0] = 0;
+        for (int i = 1; i < 366; ++i) {
+            if (travel.count(i) == false) {
+                // no need to buy ticket if it is not a travel ticket
+                dp[i] = dp[i - 1];
+            } else {
+                // see approach for explanation
+                dp[i] = min({ dp[i - 1] + costs[0], dp[max(0, i - 7)] + costs[1], dp[max(0, i - 30)] + costs[2] });
+            }
+        }
+        return dp[365];
     }
 };
