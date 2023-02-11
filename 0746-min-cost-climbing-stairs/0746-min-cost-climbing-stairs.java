f4447a0d5099b0f76bec34f9927b00b010236721
Kanav Chopra <84139025+KanavChopra@users.noreply.github.com>
2023-02-11 17:03:31 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (75.13%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -4,23 +4,15 @@
 //     2. Jump 2 step from (i - 2)th floor, dp[i] = dp[i - 2] + cost[i - 2];
 // dp[i] = min(dp[i - 1], dp[i - 2]);
 
-
 class Solution {
-    private int solve(int[] cost, int n, int[] dp) {
-        if (n == 0 || n == 1) {
-            return cost[n];
-        }
-        if (dp[n] != -1) {
-            return dp[n];
-        }
-        return dp[n] = cost[n] + Math.min(solve(cost, n - 1, dp), solve(cost, n - 2, dp));
-    }
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
         int[] dp = new int[n + 1];
-        for (int i = 0; i <= n; ++i) {
-            dp[i] = -1;
+        for (int i = 2; i <= n; ++i) {
+            int jumpOneStep = dp[i - 1] + cost[i - 1];
+            int jumpTwoStep = dp[i - 2] + cost[i - 2];
+            dp[i] = Math.min(jumpOneStep, jumpTwoStep);
         }
-        return Math.min(solve(cost, n - 1, dp), solve(cost, n - 2, dp));
+        return dp[n];
     }
 }
