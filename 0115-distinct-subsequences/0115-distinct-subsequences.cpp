e571679260f6fbcb689334bd151d2808dd685796
Kanav Chopra <84139025+KanavChopra@users.noreply.github.com>
2023-02-26 18:37:53 +0530

    Time: 25 ms (94.19%), Space: 6.6 MB (83.90%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -9,17 +9,28 @@
 class Solution {
 public:
     int numDistinct(string s, string t) {
+        // int n = s.length();
+        // int m = t.length();
+        // vector<vector<double>> dp(m + 1, vector<double>(n + 1, 0));
+        // for (int j = 0; j < n; ++j) {
+        //     dp[0][j] = 1;
+        // }
+        // for (int i = 1; i <= m; ++i) {
+        //     for (int j = 1; j <= n; ++j) {
+        //         dp[i][j] = dp[i][j - 1] + (t[i - 1] == s[j - 1] ? dp[i - 1][j - 1] : 0);
+        //     }
+        // }
+        // return (int) dp[m][n];
         int n = s.length();
         int m = t.length();
-        vector<vector<double>> dp(m + 1, vector<double>(n + 1, 0));
-        for (int j = 0; j < n; ++j) {
-            dp[0][j] = 1;
-        }
-        for (int i = 1; i <= m; ++i) {
-            for (int j = 1; j <= n; ++j) {
-                dp[i][j] = dp[i][j - 1] + (t[i - 1] == s[j - 1] ? dp[i - 1][j - 1] : 0);
+        vector<double> prev(n + 1, 1.0);
+        vector<double> curr(n + 1, 0);
+        for (long long int i = 1; i <= m; ++i) {
+            for (long long int j = 1; j <= n; ++j) {
+                curr[j] = curr[j - 1] + prev[j - 1] * (t[i - 1] == s[j - 1]);
             }
+            prev = curr;
         }
-        return (int) dp[m][n];
+        return (int) prev[n];
     }
 };
