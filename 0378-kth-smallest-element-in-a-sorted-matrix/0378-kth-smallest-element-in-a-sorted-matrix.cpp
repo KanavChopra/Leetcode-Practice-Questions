5455d281d077ec95def56c13a6e43b7dd25c746c
Kanav Chopra <84139025+KanavChopra@users.noreply.github.com>
2023-04-15 19:13:17 +0530

    Time: 32 ms (75.43%), Space: 13.2 MB (84.27%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        int n = (int) matrix.size();
-        int lo = matrix[0][0];
-        int hi = matrix[n - 1][n - 1];
-        while (lo <= hi) {
-            int mid = (lo + hi) >> 1;
-            int cnt = 0;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int low = matrix[0][0];
+        int high = matrix[n - 1][m - 1];
+        int desired = k - 1;
+        while (low <= high) {
+            int mid = (low + high) >> 1;
+            int count = 0;
             for (int i = 0; i < n; ++i) {
-                cnt += upper_bound(matrix[i].begin(), matrix[i].end(), mid) - matrix[i].begin();
+                count += upper_bound(begin(matrix[i]), end(matrix[i]), mid) - begin(matrix[i]);
             }
-            if (cnt < k) {
-                lo = mid + 1;
+            if (count <= desired) {
+                low = mid + 1;
             } else {
-                hi = mid - 1;
+                high = mid - 1;
             }
         }
-        return lo;
+        return low;
     }
 };
